7c2f9736a276bd0848fe6d4b5da23c35ae6ab09a
Arnau <arnaumoran@gmail.com>
2023-05-13 10:30:18 +0200

    bluetooth update

--- src/main/java/org/example/controllers/ServerController.java
+++ src/main/java/org/example/controllers/ServerController.java
@@ -5,11 +5,14 @@ import javax.microedition.io.Connector;
 import javax.microedition.io.StreamConnection;
 import javax.microedition.io.StreamConnectionNotifier;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ServerController {
 
-    LocalDevice lDevice;
-    StreamConnectionNotifier notifier;
+    private ArrayList<StreamConnection> connections = new ArrayList<StreamConnection>();
+    private LocalDevice lDevice;
+    private StreamConnectionNotifier notifier;
+    private volatile boolean isRunning = true;
 
     public ServerController(){
 
@@ -23,10 +26,48 @@ public class ServerController {
         String url = "btspp://localhost:" + uuid.toString() + ";name=Server";
         notifier = (StreamConnectionNotifier) Connector.open(url);
 
-        StreamConnection connection = notifier.acceptAndOpen();
+        System.out.println("Server started");
+        
+        while (isRunning) {
+            StreamConnection connection = notifier.acceptAndOpen();
+            connections.add(connection);
+            System.out.println("Client connected: " + connection);
+            Thread t = new Thread(new ClientHandler(connection));
+            t.start();
+        }
     }
 
     public void end() throws IOException {
+        isRunning = false;
+        for (StreamConnection connection : connections) {
+            connection.close();
+        }
         notifier.close();
+    }
+
+    private class ClientHandler implements Runnable {
+        private StreamConnection connection;
+
+        public ClientHandler(StreamConnection connection) {
+            this.connection = connection;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (isRunning) {
+                    // handle client requests
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    connections.remove(connection);
+                    connection.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
