190c89cbf7d69981b2111e81e922ccbff6052105
Sirkkouga <javiabellanieto2001@gmail.com>
2023-05-14 07:27:04 +0200

    cambios finales server

--- src/main/java/org/example/controllers/PlayerController.java
+++ src/main/java/org/example/controllers/PlayerController.java
@@ -1,6 +1,7 @@
 package org.example.controllers;
 import org.example.entities.Player;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -11,15 +12,19 @@ public class PlayerController {
     //Parameters
     private ArrayList<Player> players;
     private ArrayList<Player> activePlayers;
+    private ArrayList<Player> playingPlayers;
 
     //Constructors
     public PlayerController() {
         this.players = new ArrayList<>();
+        this.activePlayers = new ArrayList<>();
+        this.playingPlayers = new ArrayList<>();
     }
 
     public PlayerController(ArrayList<Player> list, ArrayList<Player> active) {
         this.players = list;
         this.activePlayers = active;
+        this.playingPlayers = new ArrayList<>();
     }
 
     //Setters
@@ -31,6 +36,8 @@ public class PlayerController {
         this.activePlayers = active;
     }
 
+    public void setPlayingPlayers(ArrayList<Player> playing) {this.playingPlayers = playing;}
+
     //Getters
     public ArrayList<Player> getPlayers() {
         return this.players;
@@ -41,16 +48,43 @@ public class PlayerController {
     }
 
     public ArrayList<Player> getActivePlayers(){
-        return this.getActivePlayers();
+        return this.activePlayers;
+    }
+
+    public ArrayList<String> getActivePlayersString(){
+        ArrayList<String> result = new ArrayList<>();
+
+        for (Player p : this.activePlayers) {
+            result.add(p.getName());
+        }
+        return result;
+    }
+
+    public ArrayList<Player> getPlayingPlayers(){
+        return this.playingPlayers;
+    }
+
+    public ArrayList<String> getPlayingPlayersString(){
+        ArrayList<String> result = new ArrayList<>();
+
+        for (Player p : this.playingPlayers) {
+            result.add(p.getName());
+        }
+        return result;
     }
 
     public int getActivePlayersnumber(){
         return this.activePlayers.size();
     }
+
+    public int getPlayingPlayersnumber(){
+        return this.playingPlayers.size();
+    }
+
     //Returns the highest score
     public int getBestScore() {
         int bestScore = 0;
-        for (Player p : this.players){
+        for (Player p : this.playingPlayers){
             if (p.getPoints() > bestScore) bestScore = p.getPoints();
         }
         return bestScore;
@@ -62,7 +96,7 @@ public class PlayerController {
 
         ArrayList<Player> result = new ArrayList<>();
 
-        for (Player p : this.players) {
+        for (Player p : this.playingPlayers) {
             if (p.getPoints() == bestScore) result.add(p);
         }
 
@@ -70,14 +104,18 @@ public class PlayerController {
     }
 
     //returns all the players ordered by their score
-    public ArrayList<Player> getBestPlayers() {
+    public ArrayList<String> getBestPlayers() {
         HashMap<Player, Integer> topPlayers = new HashMap<>();
-        for (Player p : this.players) topPlayers.put(p, p.getPoints());
+        for (Player p : this.playingPlayers) topPlayers.put(p, p.getPoints());
 
         ArrayList<Player> result = new ArrayList<>(topPlayers.keySet());
         Collections.sort(result);
 
-        return result;
+        ArrayList<String> res2 = new ArrayList<>();
+
+        for (Player p : result) res2.add(p.getName());
+
+        return res2;
     }
 
     //Operations
@@ -85,18 +123,38 @@ public class PlayerController {
         this.players.add(p);
     }
     
-    public void addPlayer(String n){this.players.add(new Player(n));}
+    public void addPlayer(String n){
+        Player p = new Player(n);
+        this.players.add(p);
+        this.activePlayers.add(p);
+    }
 
-    public void delPlayer(Player p) {
+    public void addPlayingPlayer(String n) {
+        Player p = new Player(n);
+        this.playingPlayers.add(p);
+    }
+
+    public void resetPlayer(Player p) {
         this.players.remove(p);
     }
 
-    public void delPlayer(String nom) {
+    public void delActivePlayer(String nom) {
         boolean done = false;
 
-        for (int i = 0; i < this.players.size() && !done; ++i) {
-            if (this.players.get(i).getName().equals(nom)) {
-                this.players.remove(i);
+        for (int i = 0; i < this.activePlayers.size() && !done; ++i) {
+            if (this.activePlayers.get(i).getName().equals(nom)) {
+                this.activePlayers.remove(i);
+                done = true;
+            }
+        }
+    }
+
+    public void delPlayingPlayer(String nom) {
+        boolean done = false;
+
+        for (int i = 0; i < this.playingPlayers.size() && !done; ++i) {
+            if (this.playingPlayers.get(i).getName().equals(nom)) {
+                this.playingPlayers.remove(i);
                 done = true;
             }
         }
--- src/main/java/org/example/controllers/ServerController.java
+++ src/main/java/org/example/controllers/ServerController.java
@@ -1,24 +1,90 @@
 package org.example.controllers;
 
+import org.example.entities.Player;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class ServerController {
     public static void main(String[] args) throws IOException {
+
+        PlayerController contP = new PlayerController();
+
         int portNumber = 8080;
         ServerSocket serverSocket = new ServerSocket(portNumber);
         System.out.println("Server started on port " + portNumber);
 
-        // Wait for a client to connect
-        Socket clientSocket = serverSocket.accept();
-        System.out.println("Client connected from " + clientSocket.getInetAddress().getHostAddress());
+        while (true) {
+            // Wait for a client to connect
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected from " + clientSocket.getInetAddress().getHostAddress());
+
+            //Open input and output channels
+            InputStream istream = clientSocket.getInputStream();
+            OutputStream ostream = clientSocket.getOutputStream();
+
+            //Read the next message from input
+            byte[] typebuffer = new byte[1024];
+            int bytesRead = istream.read(typebuffer);
+            String input = new String(typebuffer, 0, bytesRead);
+
+            System.out.println(input);
+
+            String [] ops = input.split(",");
+
+            //Do operation
+            switch (ops[0]) {
+                case "new":
+                    String playerName = ops[1];
+
+                    contP.addPlayer(playerName);
+
+                    break;
+
+                case "active":
+
+                    for (String s : contP.getActivePlayersString()) {
+                        byte[] playername = s.getBytes();
+                        ostream.write(playername);
+                        ostream.write((",").getBytes());
+                    }
+                    ostream.flush();
+
+                    break;
+
+                case "time":
+
+                    for (Player p : contP.getActivePlayers()) {
+                        p.setX(48.879304F);
+                        p.setY(2.357054F);
+                    }
+
+                    contP.setPlayingPlayers(contP.getActivePlayers());
+
+                    contP.setActivePlayers(new ArrayList<>());
+                    break;
+
+                case "move":
+
+                    String x = ops[1];
+                    String y = ops[2];
+
+                    for (Player p : contP.getPlayingPlayers()) {
+                        p.setX(Float.parseFloat(x));
+                        p.setY(Float.parseFloat(y));
+                    }
+            }
+        }
 
         // Process client requests
         // ...
 
         // Close the socket and server socket when done
-        clientSocket.close();
-        serverSocket.close();
+        //clientSocket.close();
+        //serverSocket.close();
     }
 }
